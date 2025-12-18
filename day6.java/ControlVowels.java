c27208ae2d461273592831d959a64f41a7f1c836
hema <hemavathianbarasu11@gmail.com>
2025-12-18 14:29:53 +0530

    im solved leet code problems

---
+++ day6.java/ControlVowels.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ControlVowels {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextInt();
+        sc.nextLine();
+        String s=sc.nextLine();
+        System.out.println(a+""+s);
+    }
+
+}
